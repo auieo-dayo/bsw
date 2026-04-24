ad76fd1ebc30d256811e57160f28e110b1019ecf
auieo-dayo <194578811+auieo-dayo@users.noreply.github.com>
2026-04-24 23:15:01 +0900

    /pの取得中...を通知しないように

--- src/discord/admin/p.js
+++ src/discord/admin/p.js
@@ -1,6 +1,7 @@
+const { MessageFlags } = require("discord.js")
 function p(bds,message,content){
-    message.reply({content:"取得中..."})
-        const json = JSON.stringify({"type":"getplayerinfo","playername":content}).replaceAll("\"","'").replaceAll("\\","\\\\'")
-        bds.sendCommand(`send "${json}"`)
+    message.reply({content:"取得中...",flags:MessageFlags.SuppressNotifications})
+    const json = JSON.stringify({"type":"getplayerinfo","playername":content}).replaceAll("\"","'").replaceAll("\\","\\\\'")
+    bds.sendCommand(`send "${json}"`)
 }
 module.exports = p
